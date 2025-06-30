39712a48b88dc1f7e968c1b9b7819737513a3b4e
Pablo Retamales <retamalespj@gmail.com>
2025-06-30 20:48:30 +0000

    fix: start date datetime
    Co-authored-by: Vindemiatrixx <Vindemiatrixx@users.noreply.github.com>

--- T3/dags/fan_engagement_dag.py
+++ T3/dags/fan_engagement_dag.py
@@ -10,7 +10,7 @@ from kafka import KafkaProducer
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2025, 7, 1),
+    'start_date': datetime.combine(datetime.today() - timedelta(days=1), datetime.min.time()),
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
